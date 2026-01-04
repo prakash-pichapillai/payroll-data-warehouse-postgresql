12313055b488d8b1be7a4e7ad49ae2bb137ec21a
PrakashPK <prakashpichaipillaipk@gmail.com>
2026-01-04 17:28:13 +0530

    Insert sample data into dim_department and dim_location

---
+++ data/sample_dim_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO dim_department (department_id, department_name)
+VALUES
+(1, 'Engineering'),
+(2, 'Sales'),
+(3, 'Finance');
+
+INSERT INTO dim_location (country_code, country_name)
+VALUES
+('USA', 'United States'),
+('CAN', 'Canada');
