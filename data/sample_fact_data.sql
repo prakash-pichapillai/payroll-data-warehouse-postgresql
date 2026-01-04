a4f5cc498c2e1e227d07a2fde0eabf9a2940d8be
PrakashPK <prakashpichaipillaipk@gmail.com>
2026-01-04 17:37:33 +0530

    Add sample payroll data to sample_fact_data.sql

---
+++ data/sample_fact_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO fact_payroll (
+    employee_id,
+    pay_period_id,
+    gross_pay,
+    total_deductions,
+    total_taxes,
+    net_pay
+)
+VALUES
+(101, 16, 15, 4153.47, 464.00, 1050.00, 2639.47),
+(102, 16, 16, 4268.84, 521.00, 1067.00, 2680.84);
